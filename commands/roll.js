ea46e0ae2a4ad7080e79b7da8fec49fd9da8e0e7
Kraw616 <jakraw616@gmail.com>
2021-12-03 11:31:09 -0500

    Updated to add individual rolls if numDice rolled > 1

--- commands/roll.js
+++ commands/roll.js
@@ -11,19 +11,54 @@ module.exports = {
     {
       let prompt = tokens[1].split("d");
 
+
       let numDice = parseInt(prompt[0]);
       let face = parseInt(prompt[1]);
 
-      let total = 0;
-
-      for(let i = 0; i < numDice; i++)
+      if(numDice > 1)
       {
-        total += Math.floor(Math.random() * (face - 1 + 1) + 1);
-      }
-      
-      message.channel.send(total);
-    }
+        let total = 0;
+        let diceRolled = [];
 
+        for(let i = 0; i < numDice; i++)
+        {
+          let dice = Math.floor(Math.random() * (face - 1 + 1) + 1);
+          diceRolled.push(dice);
+          total += dice;
+        }
+
+        let outputString = " ";
+        diceRolled.forEach(num => outputString += num.toString() + " ");
+        message.channel.send(total.toString() + " (" + outputString + ")");
+
+              /*
+              for(let i =0; i < diceRolled.length; i++)
+              {
+                if(i == (diceRolled.length-1))
+                {
+                  outputString += diceRolled[i].toString() + "";
+                }
+                outputString += diceRolled[i].toString() + ", "
+              }
+              */
+                    
+        
+              
+            
+  
+      }
+      else
+      {
+        let total = 0;
+
+        for(let i = 0; i < numDice; i++)
+        {
+          total += Math.floor(Math.random() * (face - 1 + 1) + 1);
+        }
+        
+        message.channel.send(total);
+      }
+    }
     else
     {
       message.channel.send("Nah");
