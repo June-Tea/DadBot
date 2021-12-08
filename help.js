fbba652ba4949e2e6f1b2519cfcda951551a5aa6
rnorford <90468824+rnorford@users.noreply.github.com>
2021-12-07 20:32:36 -0500

    Update help.js

--- help.js
+++ help.js
@@ -6,10 +6,14 @@ module.exports = {
 
     execute(message){
 
-            message.channel.send("!play url = Plays the song of the url provided. Must be in a voice channel!");
-            message.channel.send("!hello = Dadbot says hi!");
             message.channel.send("!advice = Gives some life advice, for all your troubles!");
+            message.channel.send("!changeprefix = Change !");
+            message.channel.send("!hello = Dadbot says hi!");
             message.channel.send("!joke = Guranteed to make you laugh with a hilariously bad joke!");
+            message.channel.send("!play url = Plays the song of the url provided. Must be in a voice channel!");
+            message.channel.send("!roll = Roll the dice!");
+            message.channel.send("!skip = Skip to the next song!");
+            message.channel.send("!stop = Shut off the music!");
 
         }
 
